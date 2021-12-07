b668fd2c20412e7c5861b5ea449fd33fda7b04a3
LiamMarega <liammarega85@gmail.com>
2021-12-07 01:20:48 -0300

    nashee

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
 
   function onSearch(ciudad) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
-    ) 
+      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`
+    )
       .then((r) => r.json())
       .then((recurso) => {
         if (recurso.main !== undefined && cities.length < 8) {
