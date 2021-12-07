44c842bfb2a7c91affae8c14a9f455269e5b6af1
LiamMarega <liammarega85@gmail.com>
2021-12-07 01:49:18 -0300

    APIKEY

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   function onSearch(ciudad) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`
     )
       .then((r) => r.json())
       .then((recurso) => {
