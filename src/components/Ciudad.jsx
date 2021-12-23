14502a604676d8954cccf4b58a680b8324ae8771
LiamMarega <liammarega85@gmail.com>
2021-12-23 01:50:44 -0300

    cambios

--- src/components/Ciudad.jsx
+++ src/components/Ciudad.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "./ciudad.module.css";
+import styled from "./Ciudad.module.css"
 
 export default function Ciudad({ city }) {
   return (
-    <div className="ciudad">
-      <div className="container">
-      <div className={styled.texto}>
-        <h2 className={styled.city}>{city.name}</h2>
-        <div className="info">
-          <div>Temperatura: {city.temp} ºC</div>
-          <div>Clima: {city.weather}</div>
-          <div>Viento: {city.wind} km/h</div>
-          <div>Cantidad de nubes: {city.clouds}</div>
-          <div>Latitud: {city.latitud}º</div>
-          <div>Longitud: {city.longitud}º</div>
+    <div className={styled.ciudad}>
+      {city ? (
+        <div className={styled.containerCiudad}>
+          <div className={styled.texto}>
+            <h2 className={styled.city}>{city.name}</h2>
+            <div className="info">
+              <div>Temperatura: {city.temp} ºC</div>
+              <div>Clima: {city.weather}</div>
+              <div>Viento: {city.wind} km/h</div>
+              <div>Cantidad de nubes: {city.clouds}</div>
+              <div>Latitud: {city.latitud}º</div>
+              <div>Longitud: {city.longitud}º</div>
+            </div>
+          </div>
+          <Link to={"/"}>
+            <button className={styled.boton}>BACK</button>
+          </Link>
         </div>
-        </div>
-        <Link to={'/'}>
-        <button className={styled.boton}>BACK</button>
+      ) : (
+        <Link to={"/"}>
+          <button className={styled.boton}>BACK</button>
         </Link>
-      </div>
+          
+      )}
     </div>
   );
 }
